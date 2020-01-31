896254c6831a6ac57a1cf7705793dddb5d7c2046
Okan Demirel <okn.118dmrl@gmail.com>
2020-01-31 23:03:16 +0300

    Her şeyi baştan yolluyorum

--- Assets/Scripts/CameraDistanceFadeOut.cs
+++ Assets/Scripts/CameraDistanceFadeOut.cs
@@ -7,7 +7,6 @@ public class CameraDistanceFadeOut : MonoBehaviour
     [SerializeField]
     GameObject object1, object2;
 
-    // Update is called once per frame
     void Update()
     {
         var distance = Vector3.Distance(object1.transform.position, object2.transform.position);
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -5,7 +5,10 @@ using System.Collections;
 
 public class Move : MonoBehaviour
 {
-    public float speed = 100;
+    [SerializeField]
+    float speed = 100;
+    float rotation;
+    public float rotationSpeed = 100.0f;
     float translationVertical, translationHorizontal;
     Rigidbody rb;
 
@@ -17,19 +20,28 @@ public class Move : MonoBehaviour
     void Update()
     {
         if (this.gameObject.CompareTag("Player1")) {
+             rotation = Input.GetAxis("HorizontalPlayer1") * rotationSpeed;
              translationVertical = Input.GetAxis("VerticalPlayer1") * speed;
-             translationHorizontal = Input.GetAxis("HorizontalPlayer1") * speed;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //Ateş et
+            }
         }
         else if (this.gameObject.CompareTag("Player2"))
         {
+             rotation = Input.GetAxis("HorizontalPlayer2") * rotationSpeed;
              translationVertical = Input.GetAxis("VerticalPlayer2") * speed;
-             translationHorizontal = Input.GetAxis("HorizontalPlayer2") * speed;
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                //Ateş et
+            }
         }
 
-        translationHorizontal *= Time.deltaTime;
         translationVertical *= Time.deltaTime;
-        
+        rotation *= Time.deltaTime;
 
-        rb.velocity = new Vector3(translationHorizontal, 0, translationVertical);
+
+        transform.Rotate(0, rotation, 0);
+        rb.velocity = new Vector3(0, 0, translationVertical);
     }
 }
