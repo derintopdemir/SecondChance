17c7d52d8aa8a72394aac0e22cdd2e321410460c
furkan-celik <game.wive@gmail.com>
2020-02-01 02:34:01 +0300

    Part Drop Off to Target Added

--- Assets/Scripts/PickupItem.cs
+++ Assets/Scripts/PickupItem.cs
@@ -7,19 +7,8 @@ public class PickupItem : MonoBehaviour
 {
     public GameObject interactTextObj;
     public Transform itemPickupLocation;
+    public Material placedMaterial, correctMaterial, wrongMaterial;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Interactable") && itemPickupLocation.childCount == 0)
@@ -31,6 +20,24 @@ public class PickupItem : MonoBehaviour
         {
             interactTextObj.SetActive(true);
             interactTextObj.GetComponent<Text>().text = "You Cannot Carry More Item";
+        }
+
+        if (other.CompareTag("FillPart"))
+        {
+            if(itemPickupLocation.childCount > 0 && other.GetComponent<PartInfo>().objId == itemPickupLocation.GetChild(0).GetComponent<PartInfo>().objId)
+            {
+                interactTextObj.SetActive(true);
+                interactTextObj.GetComponent<Text>().text = "E to Place";
+                other.gameObject.GetComponent<MeshRenderer>().enabled = true;
+                other.gameObject.GetComponent<MeshRenderer>().material = correctMaterial;
+            }
+            else
+            {
+                interactTextObj.SetActive(true);
+                interactTextObj.GetComponent<Text>().text = "No Proper Item";
+                other.gameObject.GetComponent<MeshRenderer>().enabled = true;
+                other.gameObject.GetComponent<MeshRenderer>().material = wrongMaterial;
+            }
         }
     }
 
@@ -44,6 +51,13 @@ public class PickupItem : MonoBehaviour
             other.gameObject.GetComponent<BoxCollider>().enabled = false;
             interactTextObj.SetActive(false);
         }
+        else if (other.CompareTag("FillPart") && Input.GetAxis("Interact1") > 0 && itemPickupLocation.childCount > 0 && other.GetComponent<PartInfo>().objId == itemPickupLocation.GetChild(0).GetComponent<PartInfo>().objId)
+        {
+            other.gameObject.GetComponent<MeshRenderer>().material = placedMaterial;
+            Destroy(itemPickupLocation.GetChild(0).gameObject);
+            interactTextObj.SetActive(false);
+            other.gameObject.tag = "Untagged";
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -51,6 +65,11 @@ public class PickupItem : MonoBehaviour
         if (other.CompareTag("Interactable"))
         {
             interactTextObj.SetActive(false);
+        }
+        else if (other.CompareTag("FillPart"))
+        {
+            interactTextObj.SetActive(false);
+            other.gameObject.GetComponent<MeshRenderer>().enabled = false;
         }
     }
 }
---
+++ Assets/Scripts/RemainedPartsController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RemainedPartsController : MonoBehaviour
+{
+    public Text uiText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        uiText.text = GameObject.FindGameObjectsWithTag("FillPart").Length.ToString();
+    }
+}
