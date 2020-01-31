875a426c115b470e628ffb11fda7037f6b46c740
Okan Demirel <okn.118dmrl@gmail.com>
2020-01-31 22:18:24 +0300

    Move falan eklendi işte

---
+++ Assets/Scripts/CameraDistanceFadeOut.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDistanceFadeOut : MonoBehaviour
+{
+    [SerializeField]
+    GameObject object1, object2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var distance = Vector3.Distance(object1.transform.position, object2.transform.position);
+
+        if(distance >= 10)
+        {
+            Camera.main.fieldOfView = 100;
+        }
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// A very simplistic car driving on the x-z plane.
+
+public class Move : MonoBehaviour
+{
+    public float speed = 100;
+    float translationVertical, translationHorizontal;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (this.gameObject.CompareTag("Player1")) {
+             translationVertical = Input.GetAxis("VerticalPlayer1") * speed;
+             translationHorizontal = Input.GetAxis("HorizontalPlayer1") * speed;
+        }
+        else if (this.gameObject.CompareTag("Player2"))
+        {
+             translationVertical = Input.GetAxis("VerticalPlayer2") * speed;
+             translationHorizontal = Input.GetAxis("HorizontalPlayer2") * speed;
+        }
+
+        translationHorizontal *= Time.deltaTime;
+        translationVertical *= Time.deltaTime;
+        
+
+        rb.velocity = new Vector3(translationHorizontal, 0, translationVertical);
+    }
+}
