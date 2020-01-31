7737686a0479974f4062f19b3a8e4b7fea3f2428
furkan-celik <game.wive@gmail.com>
2020-02-01 00:41:19 +0300

    Pickup code and FaceToCamera code intro

---
+++ Assets/Scripts/FaceToCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceToCamera : MonoBehaviour
+{
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        gameObject.transform.LookAt(Camera.main.transform.position * -1f);
+    }
+}
--- Assets/Scripts/PickupItem.cs
+++ Assets/Scripts/PickupItem.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickupItem : MonoBehaviour
 {
+    public GameObject interactTextObj;
+    public Transform itemPickupLocation;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +22,35 @@ public class PickupItem : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Interactable") && itemPickupLocation.childCount == 0)
+        {
+            interactTextObj.SetActive(true);
+            interactTextObj.GetComponent<Text>().text = "E";
+        }
+        else if (itemPickupLocation.childCount > 0)
+        {
+            interactTextObj.SetActive(true);
+            interactTextObj.GetComponent<Text>().text = "You Cannot Carry More Item";
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Interactable") && Input.GetAxis("Interact1") > 0 && itemPickupLocation.childCount == 0)
+        {
+            other.gameObject.transform.parent = itemPickupLocation.transform;
+            other.gameObject.transform.position = itemPickupLocation.position;
+            other.gameObject.transform.localScale *= 0.4f;
+            other.gameObject.GetComponent<BoxCollider>().enabled = false;
+            interactTextObj.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
         if (other.CompareTag("Interactable"))
         {
-            
+            interactTextObj.SetActive(false);
         }
     }
 }
