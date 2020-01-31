b434b0dff56edc64b60e2e1f0e642ca18a6af6c7
furkan-celik <game.wive@gmail.com>
2020-01-31 22:02:33 +0300

    First AgentController

---
+++ Assets/Scripts/AgentController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentController : MonoBehaviour
+{
+
+    public NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.CompareTag("Terrain"))
+            {
+                navMeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
