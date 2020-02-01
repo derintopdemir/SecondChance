03a92500b2b26498d4b2fd883addfd25d41eeda1
furkan-celik <game.wive@gmail.com>
2020-02-01 13:22:35 +0300

    BinaController

---
+++ Assets/Buildings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Building", menuName = "CustomObjects/Buildings", order = 1)]
+public class Buildings : ScriptableObject
+{
+    public enum BuildingType { Farm, Mine, Capacitor, O2Generator, Pump }
+    
+    public BuildingType buildingType;
+    public int buildingHealth;
+    public int contribution;
+    public Mesh mesh;
+
+    [Serializable]
+    public struct Material
+    {
+        [SerializeField]
+        public enum Type { O2, Water, Food, Energy, Mıneral }
+        [SerializeField]
+        public Type type;
+        [SerializeField]
+        public int quantity;
+    }
+    public List<Material> neededMaterials;
+}
---
+++ Assets/Materials.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Material", menuName = "CustomObjects/Buildings", order = 2)]
+public class Materials : ScriptableObject
+{
+
+}
