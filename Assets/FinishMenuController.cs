cd635f3bf701b2b6f75ccd0a1d59357f785c0d3c
furkan-celik <game.wive@gmail.com>
2020-02-02 12:48:04 +0300

    Menuler bitti

---
+++ Assets/FinishMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishMenuController : MonoBehaviour
+{
+    public GameObject deathMenu, winPicture, LosePicture;
+
+    public void Death()
+    {
+        deathMenu.SetActive(true);
+        Time.timeScale = 0.1f;
+        LosePicture.SetActive(true);
+    }
+
+    public void Won()
+    {
+        deathMenu.SetActive(true);
+        Time.timeScale = 0.1f;
+        winPicture.SetActive(true);
+    }
+}
